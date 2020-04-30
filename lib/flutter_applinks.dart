391fa7175fdc8e78e0a7943cf401081b1846272b
Stanley <>
2020-04-30 18:50:30 +0800

    还原安卓覆盖文件

--- lib/flutter_applinks.dart
+++ lib/flutter_applinks.dart
@@ -13,19 +13,24 @@ class FlutterApplinks {
     return version;
   }
 
+  static Future<String> get appLinks async {
+    final String appLinks = await _channel.invokeMethod('appLinks');
+    return appLinks;
+  }
+
   static Future<dynamic> addEventHandler({
     FlutterApplinksHandler openApplinks,
     FlutterAppPushTokenHandler pushToken,
   }) async {
-    _channel.setMethodCallHandler((call){
-      if("openApplinks" == call.method){
+    _channel.setMethodCallHandler((call) {
+      if ("openApplinks" == call.method) {
         String url = call.arguments["url"];
-        if(url != null && url.length > 0){
+        if (url != null && url.length > 0) {
           return openApplinks(url);
         }
-      } else if("app_push_token" == call.method){
+      } else if ("app_push_token" == call.method) {
         String token = call.arguments;
-        if(token != null && token.length > 0){
+        if (token != null && token.length > 0) {
           return pushToken(token);
         }
       }
