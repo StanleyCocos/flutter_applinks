b7dbad843705d57f3546c703a0d4d0d208f394ff
Stanley <>
2020-04-30 18:35:06 +0800

    添加推送token  回調

--- ios/Classes/FlutterApplinksPlugin.m
+++ ios/Classes/FlutterApplinksPlugin.m
@@ -30,16 +30,37 @@
 
     NSString * url = userActivity.webpageURL.absoluteString;
     [self.channel invokeMethod:@"openApplinks" arguments:@{@"url": url}];
-    
-//    if ([userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb])
-//    {
-//        NSString * url = userActivity.webpageURL.absoluteString;
-//        [self.channel invokeMethod:@"openApplinks" arguments:@{@"url": url}];
-//        if(url != nil && url.length > 0){
-//            [self.channel invokeMethod:@"openApplinks" arguments:@{@"url": url}];
-//        }
-//    }
     return YES;
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken{
+
+    NSString * token = [self getHexStringForData:deviceToken];
+    if(token != nil && token.length > 0){
+        [channel invokeMethod:"app_push_token" arguments:token];
+    }
+}
+
+
+- (NSString *)getHexStringForData:(NSData *)data
+{
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 13) {
+
+        if (![data isKindOfClass:[NSData class]]) {
+            return @"";
+        }
+        NSUInteger len = [data length];
+        char *chars = (char *)[data bytes];
+        NSMutableString *hexString = [[NSMutableString alloc]init];
+        for (NSUInteger i=0; i<len; i++) {
+            [hexString appendString:[NSString stringWithFormat:@"%0.2hhx" , chars[i]]];
+        }
+        return hexString;
+    } else {
+         NSString *myToken = [[data description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+        myToken = [myToken stringByReplacingOccurrencesOfString:@" " withString:@""];
+        return myToken;
+    }
 }
 
 @end
--- lib/flutter_applinks.dart
+++ lib/flutter_applinks.dart
@@ -3,23 +3,30 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 typedef Future<dynamic> FlutterApplinksHandler(String url);
+typedef Future<dynamic> FlutterAppPushTokenHandler(String token);
 
 class FlutterApplinks {
   static const MethodChannel _channel = const MethodChannel('flutter_applinks');
 
-  static Future<String> get appLinks async {
-    final String appLinks = await _channel.invokeMethod('appLinks');
-    return appLinks;
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
   }
 
   static Future<dynamic> addEventHandler({
     FlutterApplinksHandler openApplinks,
+    FlutterAppPushTokenHandler pushToken,
   }) async {
-    _channel.setMethodCallHandler((call) {
-      if ("openApplinks" == call.method) {
+    _channel.setMethodCallHandler((call){
+      if("openApplinks" == call.method){
         String url = call.arguments["url"];
-        if (url != null && url.length > 0) {
+        if(url != null && url.length > 0){
           return openApplinks(url);
+        }
+      } else if("app_push_token" == call.method){
+        String token = call.arguments;
+        if(token != null && token.length > 0){
+          return pushToken(token);
         }
       }
       return null;
