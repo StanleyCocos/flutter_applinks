8b7c9b4106f84ba10f23e48237d36fabc8759de6
linchuanghu <linchuanghu@addcn.com>
2021-09-02 11:48:41 +0800

    null-safety migrate

--- lib/flutter_applinks.dart
+++ lib/flutter_applinks.dart
@@ -8,33 +8,33 @@ typedef Future<dynamic> FlutterAppPushTokenHandler(String token);
 class FlutterApplinks {
   static const MethodChannel _channel = const MethodChannel('flutter_applinks');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static Future<String> get appLinks async {
-    final String appLinks = await _channel.invokeMethod('appLinks');
+  static Future<String?> get appLinks async {
+    final String? appLinks = await _channel.invokeMethod('appLinks');
     return appLinks;
   }
 
   static Future<dynamic> addEventHandler({
-    FlutterApplinksHandler openApplinks,
-    FlutterAppPushTokenHandler pushToken,
+    FlutterApplinksHandler? openApplinks,
+    FlutterAppPushTokenHandler? pushToken,
   }) async {
     _channel.setMethodCallHandler((call) {
       if ("openApplinks" == call.method) {
-        String url = call.arguments["url"];
+        String? url = call.arguments["url"];
         if (url != null && url.length > 0) {
-          return openApplinks(url);
+          return openApplinks!(url);
         }
       } else if ("app_push_token" == call.method) {
-        String token = call.arguments;
+        String? token = call.arguments;
         if (token != null && token.length > 0) {
-          return pushToken(token);
+          return pushToken!(token);
         }
       }
-      return null;
+      return null as dynamic;
     });
   }
 }
